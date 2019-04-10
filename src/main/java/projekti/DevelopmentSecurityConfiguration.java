b3318e93176ff2ef5db21db324cd6e9993772a4a
Dani Jompero <dani.jompero@gmail.com>
2019-04-10 20:43:32 +0300

    entities and repositories

--- src/main/java/projekti/DevelopmentSecurityConfiguration.java
+++ src/main/java/projekti/DevelopmentSecurityConfiguration.java
@@ -1,10 +1,14 @@
 package projekti;
 
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableJpaRepositories(basePackages = "repositories")
+@EntityScan(basePackages = "entities")
 public class DevelopmentSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
